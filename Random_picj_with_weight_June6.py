5d13b5ccf8e52897549e5fcac218f9fd8f4a3701
Rohit gorle <61993839+rohit-gorle@users.noreply.github.com>
2020-06-08 23:33:22 -0700

    June leet code challenge

---
+++ Random_picj_with_weight_June6.py
@@ -0,0 +1,17 @@
+class Solution(object):
+
+    def __init__(self, w):
+        """
+        :type w: List[int]
+        """
+        self.cdf = [0]
+        for weight in w:
+            self.cdf.append(self.cdf[-1] + weight)
+
+    def pickIndex(self):
+        """
+        :rtype: int
+        """
+        rand = random.randint(1, self.cdf[-1])
+        idx = bisect.bisect_left(self.cdf, rand)
+        return idx-1
